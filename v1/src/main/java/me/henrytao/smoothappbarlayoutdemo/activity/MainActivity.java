72dd352f0c77fd8c8b4f61df899698769d9c618a
Henry Tao <hi@henrytao.me>
2016-01-31 09:46:42 +0700

    Updated name

--- v1/src/main/java/me/henrytao/smoothappbarlayoutdemo/activity/MainActivity.java
+++ v1/src/main/java/me/henrytao/smoothappbarlayoutdemo/activity/MainActivity.java
@@ -86,13 +86,13 @@ public class MainActivity extends BaseActivity implements SimpleAdapter.OnItemCl
     List<Feature> features = new ArrayList<>();
     features.add(new Feature(GsdScrollActivity.class, "scroll"));
     features.add(new Feature(null, ""));
-    features.add(new Feature(GsdScrollEnterAlwaysActivity.class, "scroll|enterAlways"));
+    features.add(new Feature(GsdScrollEnterAlwaysActivity.class, "scroll | enterAlways"));
     features.add(new Feature(null, ""));
-    features.add(new Feature(GsdScrollEnterAlwaysCollapsedActivity.class, "scroll|enterAlwaysCollapsed"));
+    features.add(new Feature(GsdScrollEnterAlwaysCollapsedActivity.class, "scroll | enterAlwaysCollapsed"));
     features.add(new Feature(null, ""));
-    features.add(new Feature(GsdScrollExitUntilCollapsedActivity.class, "scroll|exitUntilCollapsed"));
+    features.add(new Feature(GsdScrollExitUntilCollapsedActivity.class, "scroll | exitUntilCollapsed"));
     features.add(new Feature(null, ""));
-    features.add(new Feature(GsdScrollSnapActivity.class, "scroll|snap"));
+    features.add(new Feature(GsdScrollSnapActivity.class, "scroll | snap"));
     features.add(new Feature(null, ""));
     return features;
   }
