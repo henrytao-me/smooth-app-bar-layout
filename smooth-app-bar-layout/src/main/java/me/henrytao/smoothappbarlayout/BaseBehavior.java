7164ccb82da7e239482e3c0ac56d79342c048c34
Henry Tao <hi@henrytao.me>
2016-07-08 05:26:59 +0700

    Removed unused import

--- smooth-app-bar-layout/src/main/java/me/henrytao/smoothappbarlayout/BaseBehavior.java
+++ smooth-app-bar-layout/src/main/java/me/henrytao/smoothappbarlayout/BaseBehavior.java
@@ -16,7 +16,6 @@
 
 package me.henrytao.smoothappbarlayout;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
